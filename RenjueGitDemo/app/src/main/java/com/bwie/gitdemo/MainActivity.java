4d7ed6054aa84e501e27394aa7adc6be1cf24934
unknown <Administrator@WQ-20170120WETL.(none)>
2017-05-09 08:20:37 +0800

    commit code

--- RenjueGitDemo/app/src/main/java/com/bwie/gitdemo/MainActivity.java
+++ RenjueGitDemo/app/src/main/java/com/bwie/gitdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //分支上提交
+        //hahahahaha
     }
 }
